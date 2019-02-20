2895448b7f23cc8982556784fcd3f649c0d48bb9
Frank Refol <t3knoid@live.com>
2019-02-19 23:07:53 -0500

    Added retries.

--- MagyarTV/parsers/mediaklikk.py
+++ MagyarTV/parsers/mediaklikk.py
@@ -2,40 +2,55 @@ import sys
 sys.path.append('lib')
 
 import requests
-from six.moves.urllib.parse import urljoin
+from urllib.parse import urljoin
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import getopt
 
 
 def geturi(index):
-    """ Returns the URI for a given index
-        THis parses URI from https://www.mediaklikk.hu/
+    """ Returns the M3U8 URI for a given index from https://www.mediaklikk.hu/.
+
+        An example M3U8 file looks like the following:
+        #EXTM3U
+        #EXT-X-VERSION:3
+        #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=468677,CODECS="avc1.42c00d,mp4a.40.2",RESOLUTION=320x180
+        05.m3u8
+        #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=826286,CODECS="avc1.42c01e,mp4a.40.2",RESOLUTION=480x270
+        04.m3u8
+        #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1183894,CODECS="avc1.4d401e,mp4a.40.2",RESOLUTION=640x360
+        03.m3u8
+        #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1490416,CODECS="avc1.4d4028,mp4a.40.2",RESOLUTION=854x480
+        02.m3u8
     """
 
     search_str = "\/index.m3u8"
-    high_res_m3u = "02.m3u8"
-    high_res_video = ""
+    m3u8_index = ""
+    found = False
+    retries = 0
 
     # Read index feed
-    page = requests.get(index)
-    tree = BeautifulSoup(page.text, 'html.parser')
-    scripts = tree.find_all('script')
-    script = scripts[6].text
+    while (not found) or (retries < 10):   # Retry at least 10x
+        retries += 1
+        page = requests.get(index)
+        tree = BeautifulSoup(page.text, 'html.parser')
+        scripts = tree.find_all('script')
+        script = scripts[6].text
 
-    # Split the script into a list of individual lines
-    lines = script.split('\n')
+        # Split the script into a list of individual lines
+        lines = script.split('\n')
 
-    # Get the line containing index.m3u8
-    found = [s for s in lines if (search_str in s)]
+        # Get the line containing index.m3u8
+        found = [s for s in lines if (search_str in s)]
 
-    if found:
-        found_line = found[0]
-        url_parts = found_line.split('"')
-        m3u8_index = 'https:%s' % url_parts[3].replace('\\', '')
-        high_res_video = urljoin(m3u8_index, high_res_m3u)
+        if found:
+            found_line = found[0]
+            url_parts = found_line.split('"')
+            m3u8_index = 'https:%s' % url_parts[3].replace('\\', '')
 
-    return high_res_video
+    m3u8_url = urljoin(m3u8_index,urlparse(m3u8_index).path)
 
+    return m3u8_url
 
 feed = None
 
